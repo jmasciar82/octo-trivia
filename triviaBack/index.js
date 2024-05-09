a837c4d56092ff54276a83d8318cb1f91403cb48
jmasciar82 <109538968+jmasciar82@users.noreply.github.com>
2024-05-08 23:19:49 -0300

    v4

--- triviaBack/index.js
+++ triviaBack/index.js
@@ -15,11 +15,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Middleware para seguridad de contenido
-/* app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://octo-trivia-front.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-}); */
+});
+
 
 // Rutas API RESTful
 const sala1Pregunta1Routes = require('./router/sala1/pregunta1Routes.js');
