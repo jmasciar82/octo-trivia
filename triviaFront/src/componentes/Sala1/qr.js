92a1cea2faaf229f34a996254623398bc1dd46a3
jmasciar82 <109538968+jmasciar82@users.noreply.github.com>
2024-05-09 00:13:16 -0300

    v6.1

--- triviaFront/src/componentes/Sala1/qr.js
+++ triviaFront/src/componentes/Sala1/qr.js
@@ -3,7 +3,7 @@ import QRCode from 'qrcode.react';
 
 export const QRGenerator = ({ text = 'sala1_pregunta1' }) => {
     const frontendURL = 'https://octo-trivia-front.vercel.app/index/sala/6617f798c3eb3b3b51f8df76/pregunta/6617f799c3eb3b3b51f8df78';
-    const qrText = `${frontendURL}/${text}`;
+    const qrText = `${frontendURL}`
 
     return (
         <div className="qrContainer">
