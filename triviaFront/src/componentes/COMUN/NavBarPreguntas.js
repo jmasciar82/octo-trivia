75ac2eb5baa8b4a09ad4897105f61c51859cf612
jmasciar82 <109538968+jmasciar82@users.noreply.github.com>
2024-05-11 11:03:35 -0300

    1.8

--- triviaFront/src/componentes/COMUN/NavBarPreguntas.js
+++ triviaFront/src/componentes/COMUN/NavBarPreguntas.js
@@ -1,12 +1,15 @@
 import { Button } from 'react-bootstrap';
 import './estiloBotonPreguntas.css'; // Importa el archivo CSS
+const frontendURL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_FRONT_URL : 'http://localhost:3001';
+
 
 export const NavBarPreguntas = () => {
     return (
         <div>
             <nav>
-                <Button href="http://localhost:3001/index/sala/6617f798c3eb3b3b51f8df76/pregunta/6617f799c3eb3b3b51f8df78" className="nav-button">1</Button>
-                <Button href="http://localhost:3001/index/sala/6617f798c3eb3b3b51f8df76/pregunta/66244bb203d88da06a04ae8b" className="nav-button">2</Button>
+                <Button href={`${frontendURL}/index/sala/6617f798c3eb3b3b51f8df76/pregunta/6617f799c3eb3b3b51f8df78`} className="nav-button">1</Button>
+                <Button href={`${frontendURL}/index/sala/6617f798c3eb3b3b51f8df76/pregunta/66244bb203d88da06a04ae8b`} className="nav-button">2</Button>
+                
             </nav>
         </div>
     );
