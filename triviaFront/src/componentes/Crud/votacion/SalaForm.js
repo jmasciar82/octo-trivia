a99582d97575847bb3f8dee8c0d1a048806332d6
jmasciar82 <109538968+jmasciar82@users.noreply.github.com>
2024-08-05 16:15:40 -0300

    ultimos cambios

--- triviaFront/src/componentes/Crud/votacion/SalaForm.js
+++ triviaFront/src/componentes/Crud/votacion/SalaForm.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './styles.css';
 
 const backendURL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_BACKEND_URL : 'http://localhost:5000';
 
@@ -16,39 +15,45 @@ const SalaForm = ({ salaId, onSave }) => {
                 .catch(error => {
                     console.error('Error al obtener la sala:', error);
                 });
-        } else {
-            setNombre('');
         }
     }, [salaId]);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const salaData = { nombre };
-
-        try {
-            if (salaId) {
-                await axios.put(`${backendURL}/admin/sala/${salaId}`, salaData);
-            } else {
-                await axios.post(`${backendURL}/admin/sala`, salaData);
-            }
-            onSave();
-        } catch (error) {
-            console.error('Error al guardar la sala:', error);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (salaId) {
+            // Editar sala existente
+            axios.put(`${backendURL}/admin/sala/${salaId}`, { nombre })
+                .then(response => {
+                    onSave();
+                })
+                .catch(error => {
+                    console.error('Error al editar la sala:', error);
+                });
+        } else {
+            // Crear nueva sala
+            axios.post(`${backendURL}/admin/sala`, { nombre })
+                .then(response => {
+                    onSave();
+                })
+                .catch(error => {
+                    console.error('Error al crear la sala:', error);
+                });
         }
     };
 
     return (
-        <form className="form-container" onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="form-container">
+            <h1 className="form-title">{salaId ? 'Editar Sala' : 'Crear Nueva Sala'}</h1>
             <div className="form-group">
                 <label className="form-label">Nombre:</label>
-                <input
-                    type="text"
-                    className="form-input"
-                    value={nombre}
-                    onChange={(e) => setNombre(e.target.value)}
+                <input 
+                    type="text" 
+                    value={nombre} 
+                    onChange={e => setNombre(e.target.value)} 
+                    className="form-input" 
                 />
             </div>
-            <button type="submit" className="form-button">{salaId ? 'Actualizar' : 'Crear'}</button>
+            <button type="submit" className="form-button">{salaId ? 'Guardar Cambios' : 'Crear Sala'}</button>
         </form>
     );
 };
--- triviaFront/src/componentes/Crud/votacion/SalaList.js
+++ triviaFront/src/componentes/Crud/votacion/SalaList.js
@@ -7,7 +7,7 @@ const backendURL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP
 
 const SalaList = () => {
     const [salas, setSalas] = useState([]);
-    const [editingSala, setEditingSala] = useState(false);
+    const [editingSala, setEditingSala] = useState({ isEditing: false, salaId: null });
 
     useEffect(() => {
         axios.get(`${backendURL}/admin/salas`)
@@ -30,7 +30,7 @@ const SalaList = () => {
     };
 
     const handleSave = () => {
-        setEditingSala(false);
+        setEditingSala({ isEditing: false, salaId: null });
         axios.get(`${backendURL}/admin/salas`)
             .then(response => {
                 setSalas(response.data);
@@ -43,16 +43,16 @@ const SalaList = () => {
     return (
         <div className="form-container">
             <h1 className="form-title">Lista de Salas</h1>
-            <button className="form-button" onClick={() => setEditingSala(true)}>Crear Nueva Sala</button>
-            {editingSala && (
-                <SalaForm salaId={null} onSave={handleSave} />
+            <button className="form-button" onClick={() => setEditingSala({ isEditing: true, salaId: null })}>Crear Nueva Sala</button>
+            {editingSala.isEditing && (
+                <SalaForm salaId={editingSala.salaId} onSave={handleSave} />
             )}
             <ul className="form-list">
                 {salas.map(sala => (
                     <li className="form-list-item" key={sala._id}>
                         {sala.nombre}
                         <div className="form-list-actions">
-                            <button className="form-edit-button" onClick={() => setEditingSala(sala._id)}>Editar</button>
+                            <button className="form-edit-button" onClick={() => setEditingSala({ isEditing: true, salaId: sala._id })}>Editar</button>
                             <button className="form-delete-button" onClick={() => handleDelete(sala._id)}>Eliminar</button>
                         </div>
                     </li>
