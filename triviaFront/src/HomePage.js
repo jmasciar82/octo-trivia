03d5973f9c78a9b7a53a8bdf327189c6eeac7fb6
jmasciar82 <109538968+jmasciar82@users.noreply.github.com>
2024-08-09 10:39:07 -0300

    ultimos cambios 7

--- triviaFront/src/HomePage.js
+++ triviaFront/src/HomePage.js
@@ -1,30 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './index.css';
 
 const HomePage = () => {
-    const [background, setBackground] = useState('');
-
-    const changeBackground = (imageUrl) => {
-        console.log('Cambiando fondo a:', imageUrl); // Esto debería aparecer en la consola
-        document.body.style.backgroundImage = `url(${imageUrl})`;
-        document.body.style.backgroundSize = 'cover';
-        document.body.style.backgroundRepeat = 'no-repeat';
-    };
-
     return (
         <div className="home-page">
-            {/* Botón para cambiar el fondo */}
-            <Button
-                variant="secondary"
-                onClick={() => changeBackground('triviaFront/src/assets/PLACA.jpg')}
-            >
-                Cambiar Fondo
-            </Button>
-
-            <Link className='admin-link' to='/loginAdmin'><Button>Admin</Button></Link>
-
+            <><Link className='admin-link' to='/loginAdmin'><Button>Admin</Button></Link></>
             <Card>
                 <Card.Body>
                     <Card.Title>Bienvenido al Sistema de Votaciones</Card.Title>
@@ -45,7 +27,28 @@ const HomePage = () => {
                 </Card.Body>
             </Card>
 
+           {/*  <Card>
+                <Card.Body>
+                    <Card.Title>Bienvenido a ChatBot IA</Card.Title>
+                    <Card.Text>
+                        Presione Enter para ingresar
+                    </Card.Text>
+                    <Link to='/chatBot'>
+                        <Button variant="primary">Enter</Button>
+                    </Link>
+                </Card.Body>
+            </Card>  */}
 
+           {/*  <Card>
+                <Card.Body>
+                    <Card.Title>e-Posters <br /><br /></Card.Title>
+                    <Card.Text>
+                        Presione Enter para ingresar
+                    </Card.Text>
+                    <Link to=''>
+                        <Button variant="primary">Enter</Button></Link>
+                </Card.Body>
+            </Card> */}
 
             <Card>
                 <Card.Body>
@@ -53,17 +56,18 @@ const HomePage = () => {
                     <Card.Text>
                         Presione Enter para ingresar
                     </Card.Text>
-                    <Link to='/loginHome'><Button variant="primary">Enter</Button></Link>
+                    <Link to='/loginHome'>
+                        <Button variant="primary">Enter</Button></Link>
                 </Card.Body>
             </Card>
-
             <Card>
                 <Card.Body>
                     <Card.Title>File Manager <br /><br /></Card.Title>
                     <Card.Text>
                         Presione Enter para ingresar
                     </Card.Text>
-                    <Link to='/fileManager'><Button variant="primary">Enter</Button></Link>
+                    <Link to='/fileManager'>
+                        <Button variant="primary">Enter</Button></Link>
                 </Card.Body>
             </Card>
         </div>
