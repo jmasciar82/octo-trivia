72fdc9cdd8c8d4cae0bca0ccc94c4dcc7240ec1b
jmasciar82 <109538968+jmasciar82@users.noreply.github.com>
2024-08-09 11:55:07 -0300

    ultimos cambios 8

--- triviaFront/src/HomePage.js
+++ triviaFront/src/HomePage.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './index.css';
+import backgroundImage from '../src/assets/PLACA.jpg'; // Asegúrate de que la ruta sea correcta
 
 const HomePage = () => {
     return (
-        <div className="home-page">
-            <><Link className='admin-link' to='/loginAdmin'><Button>Admin</Button></Link></>
+        <div className="home-page" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed' }}>
+            <Link className='admin-link' to='/loginAdmin'><Button>Admin</Button></Link>
             <Card>
                 <Card.Body>
                     <Card.Title>Bienvenido al Sistema de Votaciones</Card.Title>
@@ -18,7 +19,7 @@ const HomePage = () => {
             </Card>
 
             <Card>
-                <Card.Body >
+                <Card.Body>
                     <Card.Title>Bienvenido al Sistema Word Cloud</Card.Title>
                     <Card.Text>
                         Presione Enter para ingresar
@@ -27,47 +28,23 @@ const HomePage = () => {
                 </Card.Body>
             </Card>
 
-           {/*  <Card>
-                <Card.Body>
-                    <Card.Title>Bienvenido a ChatBot IA</Card.Title>
-                    <Card.Text>
-                        Presione Enter para ingresar
-                    </Card.Text>
-                    <Link to='/chatBot'>
-                        <Button variant="primary">Enter</Button>
-                    </Link>
-                </Card.Body>
-            </Card>  */}
-
-           {/*  <Card>
-                <Card.Body>
-                    <Card.Title>e-Posters <br /><br /></Card.Title>
-                    <Card.Text>
-                        Presione Enter para ingresar
-                    </Card.Text>
-                    <Link to=''>
-                        <Button variant="primary">Enter</Button></Link>
-                </Card.Body>
-            </Card> */}
-
             <Card>
                 <Card.Body>
                     <Card.Title>Acreditaciones <br /><br /></Card.Title>
                     <Card.Text>
                         Presione Enter para ingresar
                     </Card.Text>
-                    <Link to='/loginHome'>
-                        <Button variant="primary">Enter</Button></Link>
+                    <Link to='/loginHome'><Button variant="primary">Enter</Button></Link>
                 </Card.Body>
             </Card>
+
             <Card>
                 <Card.Body>
                     <Card.Title>File Manager <br /><br /></Card.Title>
                     <Card.Text>
                         Presione Enter para ingresar
                     </Card.Text>
-                    <Link to='/fileManager'>
-                        <Button variant="primary">Enter</Button></Link>
+                    <Link to='/fileManager'><Button variant="primary">Enter</Button></Link>
                 </Card.Body>
             </Card>
         </div>
