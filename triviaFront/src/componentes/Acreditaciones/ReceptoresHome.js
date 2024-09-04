e7518592749e4e5375dd2043082901da7d347168
jmasciar82 <juanpsnusa@gmail.com>
2024-09-04 11:40:40 -0300

    cambios en lista receptores 1.1

--- triviaFront/src/componentes/Acreditaciones/ReceptoresHome.js
+++ triviaFront/src/componentes/Acreditaciones/ReceptoresHome.js
@@ -1,37 +1,41 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import './loginHome.css'
+import './ReceptoresHome.css'
 
 
 const ReceptoresHome = () => {
     return (
-        <div className="home-page" id='codigo-qr'>
+        <div>
+            <div className='lista-link-container'><Link className='lista-link' to='/lista-receptores'><Button>Lista</Button></Link></div>
+            <div className="home-page" id='codigo-qr'>
 
-            <Link to='/equipment-checkout' className="card-link">
-                <Card className="clickable-card">
-                    <Card.Body>
-                        <Card.Title>Retiro<hr></hr>Receptor</Card.Title>
-                        <Card.Text>
-                            Presione retirar un receptor
-                        </Card.Text>
-                        
-                    </Card.Body>
-                </Card>
-            </Link>
+                <Link to='/equipment-checkout' className="card-link">
+                    <Card className="clickable-card">
+                        <Card.Body>
+                            <Card.Title>Retiro<hr></hr>Receptor</Card.Title>
+                            <Card.Text>
+                                Presione retirar un receptor
+                            </Card.Text>
 
-            <Link to='/return' className="card-link">
-                <Card className="clickable-card">
-                    <Card.Body>
-                        <Card.Title>Entrega<hr></hr>Receptor</Card.Title>
-                        <Card.Text>
-                            Presione devolver un receptor
-                        </Card.Text>
-                        
-                    </Card.Body>
-                </Card>
-            </Link>
+                        </Card.Body>
+                    </Card>
+                </Link>
 
+                <Link to='/return' className="card-link">
+                    <Card className="clickable-card">
+                        <Card.Body>
+                            <Card.Title>Entrega<hr></hr>Receptor</Card.Title>
+                            <Card.Text>
+                                Presione devolver un receptor
+                            </Card.Text>
+
+                        </Card.Body>
+                    </Card>
+                </Link>
+
+
+            </div>
 
         </div>
     );
